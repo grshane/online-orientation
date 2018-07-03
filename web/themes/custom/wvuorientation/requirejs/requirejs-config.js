f3b0b9be70c7175503beba14b0d3712af09ddd6b
David Shaal <david.shaal@gmail.com>
2018-07-02 21:15:56 -0400

    loading bootstrap-native directly (without requirejs)

--- web/themes/custom/wvuorientation/requirejs/requirejs-config.js
+++ web/themes/custom/wvuorientation/requirejs/requirejs-config.js
@@ -1,15 +1,14 @@
-// js/requirejs-config.js
+// requirejs/requirejs-config.js
 
 require.config({
   paths: {
-    // 'jquery': '../node_modules/jquery/dist/jquery.min',
+    'jquery': '../node_modules/jquery/dist/jquery.min',
     'lodash': '../node_modules/lodash/lodash.min',
-    // 'bootstrap-native': '../../../node_modules/bootstrap.native/dist/bootstrap-native-v4',
     'bootstrap-native': '../node_modules/bootstrap.native/dist/bootstrap-native-v4',
 
-    'main': '../components/js/main'
+    'main': '../components/js/requirejs-main'
   }
 });
-debugger;
+
 require(['main']);
 require(['bootstrap-native']);
